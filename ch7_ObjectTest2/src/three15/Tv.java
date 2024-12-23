aa4fca59e63496698d628b6dc5ce8778dd8ab58a
htfh5362 <htfh5362@naver.com>
2024-12-23 10:41:43 +0900

    아 망했네 충돌 해결 가르쳐야 하나?

--- ch7_ObjectTest2/src/three15/Tv.java
+++ ch7_ObjectTest2/src/three15/Tv.java
@@ -4,6 +4,7 @@ public class Tv {
 
 	boolean power;
 	int channel;
+	int volume;
 	// 볼륨 속성 추가
 	
 	public void power(){
@@ -19,9 +20,13 @@ public class Tv {
 	}
 	
 	
-	// 볼륨 업 기능구현
+	void volumeUp() {
+		++volume;  
+	}
 	
-	
+	void volumeDown() {
+		--volume;  
+	}
 	// 볼륨 다운 기능구현
 	
 		
--- ch7_ObjectTest2/src/three15/TvTest.java
+++ ch7_ObjectTest2/src/three15/TvTest.java
@@ -7,19 +7,19 @@ public class TvTest{
 		SilenceTv sTv = new SilenceTv();
 		
 //주석 제거하고 문제를 풀면된다
-//		
-//		for (int i = 0; i < 7; i++) {
-//			sTv.volumeUp();
-//		}
-//		
-//		System.out.println("볼륨 업 " + sTv.volume);
-//		
-//		sTv.silenceButton();
-//		
-//		System.out.println("음소거 " + sTv.volume);
-//		
-//		sTv.silenceButton();
-//		System.out.println("음소거 끔 " + sTv.volume);
+		
+		for (int i = 0; i < 7; i++) {
+			sTv.volumeUp();
+		}
+		
+		System.out.println("볼륨 업 " + sTv.volume);
+	
+		sTv.silenceButton();
+	
+		System.out.println("음소거 " + sTv.volume);
+		
+		sTv.silenceButton();
+		System.out.println("음소거 끔 " + sTv.volume);
 	}
 
 }
