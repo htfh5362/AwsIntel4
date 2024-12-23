1521f38f73aa9e65afa116baf82ed0c84f7129ad
htfh5362 <htfh5362@naver.com>
2024-12-23 15:20:26 +0900

    오버라이딩

---
+++ ch7_ObjectBasic2/src/three30/Parent.java
@@ -0,0 +1,7 @@
+package three30;
+
+public class Parent {
+
+	int x = 10;
+	
+}
---
+++ ch7_ObjectBasic2/src/three30/SuperMain1.java
@@ -0,0 +1,13 @@
+package three30;
+
+public class SuperMain1 {
+
+	public static void main(String[] args) {
+		
+		Child c = new Child();
+		c.method();
+		
+		
+	}
+	
+}
